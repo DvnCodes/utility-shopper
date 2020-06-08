6a8b564bc96c7642fe22e6d27365c0c1be5f7af7
DvnCodes <DvnCodes@users.noreply.github.com>
2020-06-08 14:39:27 +0100

    updates url

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const fetchProducts = (price = "asc", sortby = "name") => {
   return axios
-    .get("https://utility-locker-fe.herokuapp.com/api/products", {
+    .get("https://utility-locker-be.herokuapp.com/api/products", {
       params: {
         price,
         sortby,
